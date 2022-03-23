e20ac5c8476057716761d9d06849eec5c83dbe75
Ernesto Bossi <bossi.ernestog@gmail.com>
2022-03-23 00:03:13 -0300

    add scripts

---
+++ run_adt.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Compiling ADT Example"
+echo "--------------------"
+
+make
+
+echo "--------------------"
+
+echo "Running ADT Example"
+
+echo "--------------------"
+
+./bin/adt
---
+++ run_tests.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Compiling ADT Example"
+echo "--------------------"
+make test
+
+echo "--------------------"
+
+echo "Runnung ADT Tests"
+echo "--------------------"
+./bin/adt.test
